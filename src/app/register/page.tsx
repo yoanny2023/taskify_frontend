cf5a44842dc29f838da1789a936b272a1b82bf13
yoanny <manuelvaal2021@gmail.com>
2026-01-19 16:36:22 +0100

    remove padding on forms

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -4,7 +4,7 @@ import SignUpForm from '@/components/register/SignUpForm'
 function Register() {
   return (
      <Pagina className="flex justify-center items-center">
-      <div className="w-full max-w-xs sm:px-3 sm:max-w-md">
+      <div className="w-full max-w-sm sm:px-3 sm:max-w-md">
         <SignUpForm /> 
       </div>
     </Pagina>
